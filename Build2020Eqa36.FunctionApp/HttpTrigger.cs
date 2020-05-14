e75a04cdbef329fbaee7aa702f1a1dc1c888e37d
Justin Yoo <justin.yoojh@gmail.com>
2020-05-14 11:48:08 +0900

    Initial code commit

---
+++ Build2020Eqa36.FunctionApp/HttpTrigger.cs
@@ -0,0 +1,36 @@
+using System.IO;
+using System.Threading.Tasks;
+
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Extensions.Logging;
+
+using Newtonsoft.Json;
+
+namespace Build2020Eqa36.FunctionApp
+{
+    public static class HttpTrigger
+    {
+        [FunctionName("HttpTrigger")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            log.LogInformation("C# HTTP trigger function processed a request.");
+
+            string name = req.Query["name"];
+
+            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            dynamic data = JsonConvert.DeserializeObject(requestBody);
+            name = name ?? data?.name;
+
+            string responseMessage = string.IsNullOrEmpty(name)
+                ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
+                : $"Hello, {name}.";
+
+            return new OkObjectResult(responseMessage);
+        }
+    }
+}
